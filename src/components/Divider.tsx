545ff354b7667c2142ad965e6bc1649692236ddc
Prasanna Dangol <1905915@kiit.ac.in>
2025-05-26 13:32:50 +0545

    Add Divider component

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -7,7 +7,7 @@ interface DividerProps {
 
 const Divider = ({ className }: DividerProps) => {
   return (
-    <div className={cn("py-8 flex justify-center", className)}>
+    <div className={cn("py-2 flex justify-center", className)}>
       <div className="w-24 h-1 rounded-full bg-khum-primary/30"></div>
     </div>
   );
